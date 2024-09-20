39b8a0492108268b5fb564728520bb9f6eb950d8
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:12 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux')
 {
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/luarocksport.git'), string(name: 'PORT_DESCRIPTION', value: '' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/luarocksport.git'), string(name: 'PORT_DESCRIPTION', value: '' )]
   }
 }
